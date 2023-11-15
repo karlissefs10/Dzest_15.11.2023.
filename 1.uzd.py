2f3fbdc6055269820c5b3d79b8cdfdaf1a47273f
karlissefs10 <karlissefs10>
2023-11-15 22:42:31 +0200

    Pievienoties 1.uzd.py

---
+++ 1.uzd.py
@@ -0,0 +1,16 @@
+# Izveidojam daudzrindu komentāru
+"""
+Izveidot programmu, kura prasa lietotājam ievadīt divus skaitļus un aprēķina to summu izvadot terminālī.
+"""
+
+a = int(input("Ievadi pirmo skaitli"))
+b = int(input("Ievadi otro skaitli"))
+c = a + b
+
+print(input("Ievadi pirmo skaitli"))
+c = a + b
+print(input("Ievadi otro skaitli"))
+
+
+
+
